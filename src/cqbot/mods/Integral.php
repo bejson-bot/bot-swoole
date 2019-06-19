90f890261d3830c662641278e7bbec19dc10ece1
ImDong <www@qs5.org>
2019-06-19 10:44:46 +0800

    添加 主动奇遇

--- src/cqbot/mods/Integral.php
+++ src/cqbot/mods/Integral.php
@@ -161,10 +161,24 @@ class Integral extends ModBase
      */
     public function message(string $content, string $type): bool
     {
-        if ($value = (Cache::get($this->key) ?? [])[$this->data['user_id']]) {
-            echo "{$type} => {$content}\n";
-            // 给用户加分
-            $this->add($value);
+        // 默认所有消息都会触发 奇遇
+        if ($type == 'all') {
+            // 开通积分才会有奇遇
+            if ((Cache::get($this->key) ?? [])[$this->data['user_id']]) {
+                // 字数也决定触发几率
+                $msg_len = strlen($content);
+
+                // 暂定 只有12-120个字符才触发奇遇 起码4个汉字
+                if ($msg_len < 12 || $msg_len > 120) {
+                    // 超出范围直接加分就好
+                    self::change($this->data['self_id'], $this->data['user_id'], 1, $this->data['group_id']);
+
+                    return false;
+                }
+
+                // 尝试触发奇遇
+                $this->adventure();
+            }
         }
 
         return false;
@@ -173,7 +187,6 @@ class Integral extends ModBase
     /**
      *  查询积分
      *
-     * @param array $it
      * @return void
      */
     public function query()
@@ -199,20 +212,49 @@ class Integral extends ModBase
     }
 
     /**
-     * 给用户添加积分
+     * 改动用户积分
      *
-     * @param int $value
-     * @return void
+     * @param int      $self_id
+     * @param int      $user_id 用户QQ号码
+     * @param int      $value 积分变动值 +1 / -1
+     * @param int|null $group_id 群号码
+     * @return int|bool     变动后积分值 / 失败返回false
      */
-    public function add(int $value)
+    public static function change(int $self_id, int $user_id, int $value, int $group_id = null)
     {
-        // 触发随机事件 10%
-        $add = 1; // 默认是正常的 加一分
+        // 改动 个人积分 还是 群内积分
+        if (!is_null($group_id)) {
+            $key = sprintf('Integral:%s:%s', $self_id, $group_id);
 
-        // 字数也决定触发几率
-        // $rule = strlen()
+            // 群没开通 或者 此人没开通 都返回 false
+            if ($info = Cache::get($key)) return false;
+            if (!isset($info[$user_id])) return false;
 
-        if (rand(1, 100) < 10) {
+            // 对积分加减
+            Cache::appendKey($key, $user_id, $info[$user_id] + $value);
+
+            return Cache::get($key)[$user_id];
+        }
+
+        return false;
+    }
+
+    /**
+     * 奇遇事件
+     *
+     * @param int  $max
+     * @param bool $isProcessing 是否马上处理
+     * @return array
+     */
+    public function adventure(int $max = 10, bool $isProcessing = true): array
+    {
+        // 默认返回值
+        $ret_data = [
+            'active' => false,
+        ];
+
+        // 有 10% 的几率触发奇遇
+        if (rand(1, 100) < $max) {
 
             // 取出奇遇事件列表
             $adventure = Cache::get('Integral:Adventure');
@@ -231,6 +273,14 @@ class Integral extends ModBase
             // 生成积分
             $add = isset($info['range']['1']) ? rand($info['range']['0'], $info['range']['1']) : $info['range']['0'];
 
+            // 保存奇遇事件
+            $ret_data = [
+                'active' => true,
+                'rand' => $rand,
+                'add' => $add,
+                'info' => $info
+            ];
+
             // 生成事件
             $msg = sprintf(
                 '[奇遇] %s 刚刚 %s, 积分 %+d',
@@ -239,11 +289,66 @@ class Integral extends ModBase
                 $add
             );
 
-            // 发送消息
-            $this->reply($msg);
+            // 如果立即处理发消息加积分
+            if ($isProcessing) {
+                $this->reply($msg);
+                self::change($this->data['self_id'], $this->data['user_id'], $add, $this->data['group_id']);
+            }
         }
 
-        // 保存积分
-        Cache::appendKey($this->key, $this->data['user_id'], $value + $add);
+        return $ret_data;
+    }
+
+    /**
+     * 主动触发奇遇 有惩罚机制
+     */
+    public function activeAdventure()
+    {
+        // 随机一个数
+        $value = rand(10, 50);
+
+        // 小于这个数 不奇遇 还扣钱
+        if ($value < 25) {
+            // 生成事件
+            $msg = sprintf(
+                '[主动奇遇] %s 试图触发奇遇，可惜被运气不佳，碰到个老骗子，反被骗走 %d 积分。',
+                CQ::at($this->data['user_id']),
+                -$value
+            );
+
+            // 修改积分
+            self::change($this->data['self_id'], $this->data['user_id'], -$value, $this->data['group_id']);
+
+            // 发送消息
+            $this->reply($msg);
+        } else {
+            // 正常奇遇
+            $adventure = $this->adventure(30 + $value, false);
+
+            if (!$adventure['active']) {
+                // 生成事件
+                $msg = sprintf(
+                    '[主动奇遇] %s 试图触发奇遇，并被老神仙索要 %d 积分，苦苦等待后，居然什么也没发生？',
+                    CQ::at($this->data['user_id']),
+                    -$value
+                );
+
+                // 修改积分
+                self::change($this->data['self_id'], $this->data['user_id'], -$value, $this->data['group_id']);
+            } else {
+                // 生成事件
+                $msg = sprintf(
+                    '[主动奇遇] %s 试图触发奇遇，并被老神仙索要 %d 积分，苦苦等待后，居然 %s，获得 %+d 积分，最终积分 %+d',
+                    CQ::at($this->data['user_id']),
+                    -$value,
+                    $adventure['info']['msg'],
+                    $adventure['add'],
+                    -$value + $adventure['add']
+                );
+
+                // 修改积分
+                self::change($this->data['self_id'], $this->data['user_id'], -$value + $adventure['add'], $this->data['group_id']);
+            }
+        }
     }
 }
